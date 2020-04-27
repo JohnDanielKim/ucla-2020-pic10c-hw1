b81cbabbfeec5e5086876210d45d40f4eb68ffdb
Daniel Kim <dk@daniel-macbook-pro-2018.local>
2020-04-26 19:48:31 -0700

    First commit: all instructor-given code pasted in and all program files ready to be written (main.cpp file renamed to siete-y-medio.cpp)

---
+++ hw1-versioncontrol/Hand.cpp
@@ -0,0 +1,9 @@
+//
+//  Hand.cpp
+//  hw1-versioncontrol
+//
+//  Created by Daniel Kim on 4/26/20.
+//  Copyright © 2020 John Kim. All rights reserved.
+//
+
+#include "Card.h"
---
+++ hw1-versioncontrol/siete-y-medio.cpp
@@ -0,0 +1,34 @@
+//
+//  siete-y-medio.cpp
+//  hw1-versioncontrol
+//
+//  Created by Daniel Kim on 4/26/20.
+//  Copyright © 2020 John Kim. All rights reserved.
+//
+
+#include <stdio.h>
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <ctime>
+#include <cstdlib>
+#include "Card.h"
+
+using namespace std;
+
+// Global constants (if any)
+
+
+// Non member functions declarations (if any)
+
+
+// Non member functions implementations (if any)
+
+
+// Stub for main
+int main(){
+	/* --STATEMENTS-- */
+	return 0;
+}
