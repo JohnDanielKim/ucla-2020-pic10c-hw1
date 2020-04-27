2cfb47603674c38ec422b3aefc192c9875f20bdd
Daniel Kim <dk@daniel-macbook-pro-2018.local>
2020-04-26 19:27:10 -0700

    Initial Commit

---
+++ hw1-versioncontrol/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  hw1-versioncontrol
+//
+//  Created by Daniel Kim on 4/26/20.
+//  Copyright © 2020 John Kim. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+	return 0;
+}
